ad49945aa0fe8c99bec2cce74925d32fb4723f0f
MatrixMike <matroxmike@icloud.com>
2018-02-20 17:27:32 +1100

    to allow compilation - looking for other missing files

---
+++ src/temp_include.h
@@ -0,0 +1,18 @@
+#define GDK_0 0
+#define GDK_1 1
+#define GDK_2 2
+#define GDK_3 3
+#define GDK_4 4
+#define GDK_5 5
+#define GDK_6 6
+#define GDK_7 7
+#define GDK_8 8
+#define GDK_9 9
+
+#define GDK_KP_0 0 
+#define GDK_KP_9 9 
+#define GDK_BackSpace 10 // all these just temporary
+// to compile and find other build errors
+#define GDK_Return    10
+#define GDK_KP_Enter  10
+#define GDK_Tab       10
